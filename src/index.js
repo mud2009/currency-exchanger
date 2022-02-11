ed2d741266ec4a266e4393218d7edc7ce487ae9d
Will Greenberg <greenberg.will@gmail.com>
2022-02-11 09:17:21 -0800

    add click function to index.js to gather form entries

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css'; 
+// import Exchange from './js/service.js';
+
+$(document).ready(function(){
+  $('#exchange').on("click", function(){
+    let dollars = $("#dollars").val();
+    let currency = $('#currency').val();
+    console.log(dollars);
+    console.log(currency);
+  });
+});
