15e7d078dc027f15189c2ebdf328c705b925146f
Will Greenberg <greenberg.will@gmail.com>
2022-02-11 14:06:25 -0800

    still working on error handling, statustext returning an empty string

--- src/js/service.js
+++ src/js/service.js
@@ -1,15 +1,18 @@
 export default class Exchange {
   static async getExchange() {
     try{
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/0fd51678aa18dc6ef7944444/latest/USD`);
       if (!response.ok) {
+        console.log(response);
+        console.log(response.readyState);
         throw Error(response.statusText);
       }
       return response.json();
     }
     catch(error){
-      console.log(error);
-      return error.message;
+      console.log(error.message);
+      return error;
     }
   }
 }
+// ${process.env.API_KEY}
