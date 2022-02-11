8338e9b37bc8087911bced9d7311002287b8af3b
Will Greenberg <greenberg.will@gmail.com>
2022-02-11 09:38:13 -0800

    add clearforms function, show and hide output and input methods

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css'; 
 // import Exchange from './js/service.js';
 
+function clearForms() {
+  $("#dollars").val("");
+  $("#currency").val("");
+}
+
 $(document).ready(function(){
   $('#exchange').on("click", function(){
     let dollars = $("#dollars").val();
-    let currency = $('#currency').val();
+    let currency = $("#currency").val();
+    clearForms();
     console.log(dollars);
     console.log(currency);
+    $('#userInput').hide();
+    $('#output').show();
   });
 });
